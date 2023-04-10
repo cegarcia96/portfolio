eaad9dceb775619d6ebe7b854dd7f5e2b64d2d49
Chris Garcia <garciac9203@gmail.com>
2023-04-10 14:54:07 -0700

    updated links

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About({aboutRef}) {
           I graduated with a B.S. in Chemistry from UCI, and previously worked in environmental compliance. With a background in science I enjoy applying the logical thinking and analytical skills I developed to solve complex problems. I was drawn to software engineering because of the ever-changing nature of the industry and the opportunities for lifelong learning. Here you'll find descriptions of my projects and links to the codebase for each one.
         </p>
         <p>
-          Check out my <a href="https://www.linkedin.com/in/christopher-garcia96/">LinkedIn</a>, <a href="https://github.com/cegarcia96">GitHub</a>, and <a href="https://drive.google.com/file/d/1CqEOCU_nuHKAVDi_v8g5Qo2cCINSXOwT/view?usp=sharing">resume</a> at the links below.
+          Check out my <a href="https://www.linkedin.com/in/christopher-garcia96/">LinkedIn</a>, <a href="https://github.com/cegarcia96">GitHub</a>, and <a href="https://drive.google.com/file/d/1ml4lJDN3IhzOYOqz2HMEYk3AuL4NbDuN/view?usp=sharing">resume</a> at the links below.
         </p>
         <div className="icon-container">
           <a href="https://www.linkedin.com/in/christopher-garcia96/">
